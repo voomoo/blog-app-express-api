3ff7bf3911b1695c69e68cb10f1715f11b061c12
voomoo <rakib.7896@gmail.com>
2022-09-25 11:31:35 +0600

    Connected to local DB and added colors package for better conosles

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const blogRoutes = require("./routes/blogs.routes");
 const morgan = require("morgan");
+const connectDB = require("./config/db.config");
+const colors = require("colors");
 
 //Load env vars
 dotenv.config({
     path: "./config/config.env",
 });
+
+connectDB();
 
 const app = express();
 
@@ -21,6 +25,15 @@ const PORT = process.env.PORT || 5000;
 app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
     )
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red.underline);
+
+    //close server and exit process
+    ServiceWorkerRegistration.close(() => process.exit(1));
+});
